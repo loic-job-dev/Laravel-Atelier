52970f3bb0fc6f5b9de469c8e617330dfcc5bc81
theoDS-campus <etack8r@gmail.com>
2025-07-23 14:43:12 +0200

    -add footer

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,17 +23,3 @@ class ProductController extends Controller
 
 }
 
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Illuminate\View\View;
-
-class ProductController extends Controller
-{
-
-    public function showProduct(int $id): View {
-        return view('product', ['id' => $id]);
-    }
-
-}
