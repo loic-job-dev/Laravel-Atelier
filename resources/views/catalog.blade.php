d470552bdb974ef21afbe16dadf3523fd50fb851
theoDS-campus <etack8r@gmail.com>
2025-07-23 15:22:17 +0200

    layout done

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalogue de Produits</title>
-
-    <!-- Bootstrap CSS CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    
-    <!-- Optionnel : Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
-</head>
-
-<body>
-
+@extends("layout")
+@section("content")
     <div class="container mt-5">
         <h1 class="text-center mb-4">Catalogue de Produits</h1>
         
@@ -103,7 +88,4 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zyTK5d7D5r5cr6hF6pdkDp3g3iB9LsdNf6A2Ykc1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0py+gJ13yVxll9Q3zV0w+4D2js/0hQx5dG2iwpEapUmN/5K3" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0py+gJ13yVxll9Q3zV0w+4D2js/0hQx5dG2iwpEapUmN/5K3" crossorigin="anonymous"></script>
-@include("footer")
-</body>
-
-</html>
+@endsection
