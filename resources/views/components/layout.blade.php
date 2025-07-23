dab4c34fc18cc3ac378e8c12cb183b6307501993
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-07-23 16:14:30 +0200

    app.css linked with layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,9 +2,8 @@
     <head>
         <meta charset="utf-8">
         <title>L'atelier. Votre boutique de parfums de luxe.</title>
-        <link rel="stylesheet" href="./ressources/css/app.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-        <script src="./ressources/css/app.js"></script>
     </head>
     <body>
         
