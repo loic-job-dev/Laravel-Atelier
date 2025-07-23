048c38b6df3d83eb76d42822755147ebb84290d4
Sinan Demiroz <ton@email.com>
2025-07-23 14:03:26 +0200

    rebase sinan

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @extends('/components/layout')
 @section("content")
 
@@ -50,3 +51,16 @@
     
 
 @endsection
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Home PAGE</h1>
+</body>
+</html>
+>>>>>>> 561634d (rebase sinan)
