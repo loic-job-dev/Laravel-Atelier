688b1143029092fb8a9497526d682ea4b23fc927
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-07-23 16:27:47 +0200

    refacto controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,11 @@ use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-
+    public function index()
+    {
+        return view('/product/catalog');
+    }
+    
     public function show(int $id): View {
         return view('/product/product-details', ['id' => $id]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BasketController;
 
-
-use App\Http\Controllers\CatalogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Routes for product
 
 Route::get('/product/{id}', [ProductController::class, 'show']);
 
-Route::get('/catalog', [CatalogController::class, 'index']);
+Route::get('/catalog', [ProductController::class, 'index']);
+
+
 
 Route::get('/' , function () {
     return view('homepage');
 });
 
+
+//Routes for basket
+
+Route::get('/basket', [BasketController::class, 'show']);
