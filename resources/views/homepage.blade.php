dc9747d847623e3ffe5b7fb5738f16a44974d9f7
Sinan Demiroz <ton@email.com>
2025-07-24 11:14:27 +0200

    Liens mise à jour

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,25 +1,52 @@
 @extends('/components/layout')
 @section("content")
-    <h1> Nouvelles Fragrances</h1>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce obsession">
-    <h2>Douce Obsession. Eau de Parfum </h2>
-    <p> Douce Obsession  est une véritable invitation au voyage des sens, où se mêlent la tendresse des fleurs blanches et la chaleur enveloppante des accords gourmands. Dès la première vaporisation, un souffle de néroli et de fleur d’oranger éveille les émotions, suivi d’un cœur sensuel de pivoine crémeuse et de rose centifolia. En fond, la vanille de Madagascar, la fève tonka et le bois de santal apportent une douceur addictive, tandis qu’une pointe subtile de muscs blancs laisse un sillage soyeux et persistant.</p>
-    <button>AJOUTER AU PANIER</button>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce Obsession">
-    <h2>Euphorie du Sud. Eau de Parfum </h2>
-    <p>Euphorie du Sud  est un parfum lumineux qui capture l’âme d’une côte méditerranéenne baignée de lumière dorée. Dès la première vaporisation, les agrumes pétillants de bergamote italienne et d’orange douce se révèlent comme une brise fraîche sur la peau. En son cœur, un bouquet solaire de fleur d’oranger et de jasmin s’épanouit, évoquant les orangers en fleur et la douceur des après‑midi d’été. Le sillage s’achève sur un fond chaleureux d’ambre solaire, de musc blanc et de bois de cèdre, laissant une empreinte radieuse et persistante.</p>
-    <button>AJOUTER AU PANIER</button>
-    <h1>Nos Best Sellers</h1>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce obsession">
-    <h2>Rose Majesté. Eau de Parfum</h2>
-    <button>AJOUTER AU PANIER</button>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce obsession">
-    <h2>Brise de Jasmin. Eau de Parfum</h2>
-    <button>AJOUTER AU PANIER</button>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce obsession">
-    <h2>Lagune d'Iris. Eau de Parfum</h2>
-    <button>AJOUTER AU PANIER</button>
-    <img src="/pictures/exemple.jpg" alt="Parfum douce obsession">
-    <h2>Soleil d'Ete. Eau de Parfum</h2>
-    <button>AJOUTER AU PANIER</button>
+
+<h1 class="text-center my-5">Nouvelles Fragrances</h1>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum Douce Obsession" class="img-fluid mb-2">
+      <h2>Douce Obsession. Eau de Parfum</h2>
+      <p>Douce Obsession est une véritable invitation au voyage des sens, où se mêlent la tendresse des fleurs blanches et la chaleur enveloppante des accords gourmands...</p>
+      <a href={{url ('/product/1') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+
+    <div class="col-md-12 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum Euphorie du Sud" class="img-fluid mb-2">
+      <h2>Euphorie du Sud. Eau de Parfum</h2>
+      <p>Euphorie du Sud est un parfum lumineux qui capture l’âme d’une côte méditerranéenne baignée de lumière dorée...</p>
+      <a href={{url ('/product/2') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+  </div>
+</div>
+
+    <h1 class ="text-center my-5">Nos Best Sellers</h1>
+    <div class="container mt-5">
+    <div class="row">
+    <div class="col-md-6 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum"  class="img-fluid">
+      <h2>Rose Majesté. Eau de Parfum</h2>
+      <a href={{url ('/product/3') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+
+    <div class="col-md-6 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum" class="img-fluid">
+      <h2>Brise de Jasmin. Eau de Parfum</h2>
+       <a href={{url ('/product/4') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+
+    <div class="col-md-6 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum" class="img-fluid">
+      <h2>Lagune d'Iris. Eau de Parfum</h2>
+       <a href={{url ('/product/5') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+
+    <div class="col-md-6 mb-4">
+      <img src="/pictures/exemple.jpg" alt="Parfum" class="img-fluid">
+      <h2>Soleil d'Ete. Eau de parfum</h2>
+       <a href={{url ('/product/6') }} class="btn btn-primary">AJOUTER AU PANIER</a>
+    </div>
+    
+
 @endsection
