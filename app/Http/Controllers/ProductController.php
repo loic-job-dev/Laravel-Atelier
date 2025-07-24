737d4fa6b9ae5adab3af58dba3ee9ff85ed8dd05
Sinan Demiroz <ton@email.com>
2025-07-24 16:17:45 +0200

    Ajout RAW DB Sinan

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function indeex()
+    {
+        return view('/product/catalog');
+    }
+    
+    public function show(int $id): View {
+        return view('/product/product-details', ['id' => $id]);
+    }
+     public function index() {
+        $products = DB::tables('products');
+        return view('/product/product-details' , ['products' => $products]);
+    }
+
+}
+
