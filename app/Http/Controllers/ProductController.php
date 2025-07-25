1b04e2d732fc33ab76db4aa89f2d31c350247be0
loic jobard <loic-jobard@le-campus-numerique.fr>
2025-07-25 09:29:27 +0200

    fix merge bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,33 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 
-class ProductController extends Controller {
+class ProductController extends Controller
+{
 
-
-    public function formatPrice (float $price):string {
-        return number_format($price / 100, 2, ',', ' ') . " €";
-    }
     public function index()
 
     {
-        $products= DB::select('select * from products');
+        $products = DB::select('select * from products');
 
         return view('/product/catalog', ['products' => $products]);
     }
-    
-    public function show(int $id): View {
+
+    public function show(int $id): View
+    {
+        $index = 1;
+        foreach (Product::all() as $product) {
+
+
+            echo($product->name);
+
+            $index++;
+        }
 
 
         $products = DB::select('select * from products where id = ' . $id);
         $category = DB::select('select name from categories where id =' . $products[0]->category_id);
 
-        return view('/product/product-details', ['id' => $id, 
-                                                            'price' => number_format($products[0]->price_large / 100, 2, ',', ' '),
-                                                            'product' => $products[0],
-                                                            'category' => $category[0],
-                                                            ]);
+        return view('/product/product-details', [
+            'id' => $id,
+            'price' => number_format($products[0]->price_large / 100, 2, ',', ' '),
+            'product' => $products[0],
+            'category' => $category[0],
+        ]);
     }
 }
-
--- resources/views/product/product-details.blade.php
+++ resources/views/product/product-details.blade.php
@@ -1,51 +1,61 @@
 @extends("/components/layout")
 @section("content")
-        <h1>Fiche du produit {{ $id }}</h1>
+<h1>Fiche du produit {{ $id }}</h1>
 
-        <h3>Boutique / {{ $category -> name }} / {{ $product -> name }}</h3>
-
-        <img src="/pictures/exemple.jpg" alt="picture exemple"/>
-
-        <h1>{{ $product -> name }} - {{ $category -> name }}</h1>
-        <h1>{{ $price }} €</h1>
-
-        <h2>SIZE</h2>
-        <div>
-            <button>Small (50cL)</button>
-            <button>Large (100cL)</button>
+<h3>Boutique / {{ $category -> name }} / {{ $product -> name }}</h3>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-7 ml-5">
+            <img src="/pictures/exemple.jpg" class="img-fluid" alt="picture exemple" />
         </div>
-
-        <button>AJOUTER AU PANIER</button>
-
-        <p>{{ $product -> description }}</p>
-
-        <hr>
-
-        <h2>Notes olfactives</h2>
-        <div>
-            <h3>Notes de tête</h3>
-            <h3>{{ $product -> head_notes }}</h3>
+        <div class="col-3">
+            <img src="/pictures/exemple.jpg" class="img-fluid mb-5" alt="picture exemple" />
+            <img src="/pictures/exemple.jpg" class="img-fluid" alt="picture exemple" />
         </div>
-        <div>
-            <h3>Notes de coeur</h3>
-            <h3>{{ $product -> heart_notes }}</h3>
-        </div>
-        <div>
-            <h3>Notes de fond</h3>
-            <h3>{{ $product -> deep_notes }}</h3>
-        </div>
+    </div>
+</div>
+        <h1 class="d-inline">{{ $product -> name }} - </h1>
+        <h2 class="d-inline"><em>{{ $category -> name }}</em></h2>
+<h2>{{ $price }} €</h3>
 
-        <hr>
+<h3>SIZE</h2>
+<div>
+    <button>Small (50cL)</button>
+    <button>Large (100cL)</button>
+</div>
 
-        <h2>Profil olfactif</h1>
-        <h3>Intensité {{ $product -> intensity }}</h2>
-        <h3>Sillage {{ $product -> track }}</h2>
+<button>AJOUTER AU PANIER</button>
 
-        <hr>
+<p>{{ $product -> description }}</p>
 
-        <h2>Histoire et ingrédients</h1>
+<hr>
+
+<h2>Notes olfactives</h2>
+<div>
+    <h3>Notes de tête</h3>
+    <h3>{{ $product -> head_notes }}</h3>
+</div>
+<div>
+    <h3>Notes de coeur</h3>
+    <h3>{{ $product -> heart_notes }}</h3>
+</div>
+<div>
+    <h3>Notes de fond</h3>
+    <h3>{{ $product -> deep_notes }}</h3>
+</div>
+
+<hr>
+
+<h2>Profil olfactif</h1>
+    <h3>Intensité {{ $product -> intensity }}
+</h2>
+<h3>Sillage {{ $product -> track }}</h2>
+
+    <hr>
+
+    <h2>Histoire et ingrédients</h1>
         <p>{{ $product -> history }}</p>
 
         <h3>INGREDIENTS</h3>
         <p>{{ $product -> ingredients }}</p>
-@endsection
+        @endsection
